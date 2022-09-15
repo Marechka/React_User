de48ded2f6f088fad7fcd58eb52c826c7ed18099
Masha <39933741+Marechka@users.noreply.github.com>
2022-09-15 13:41:00 -0700

    Used PORTAL for Modal, used Ref for reading value

--- 01-starting-project/src/components/Users/AddUser.js
+++ 01-starting-project/src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
@@ -6,14 +6,19 @@ import ErrorModal from "../UI/ErrorModal";
 import Wrapper from '../Helpers/Helper';
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState("");
-  const [enteredAge, setEnteredAge] = useState("");
+    const nameInputRef = useRef();
+    const ageInputRef = useRef();
+
+//   const [enteredName, setEnteredUsername] = useState("");
+//   const [enteredUserAge, setEnteredAge] = useState("");
   const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
+    const enteredName = nameInputRef.current.value;
+    const enteredUserAge= ageInputRef.current.value
     // validation
-    if (enteredUsername.trim() === 0 || enteredAge.trim() === 0) {
+    if (enteredName.trim() === 0 || enteredUserAge.trim() === 0) {
       /// return stops from following code execusion
       setError({
         title: 'Invalid input',
@@ -22,25 +27,32 @@ const AddUser = (props) => {
       return;
     }
     // plus enforces conversion from string to a number
-    if (+enteredAge < 1) {
+    if (+enteredUserAge < 1) {
     setError({
         title: "Invalid age",
         message: "Please enter valid age"
     })
       return;
     }
-    props.onAddUser(enteredUsername, enteredAge);
-    setEnteredUsername("");
-    setEnteredAge("");
+    props.onAddUser(enteredName, enteredUserAge);
+    // setEnteredUsername("");
+    // setEnteredAge("");
+
+    // since using Refs doing different valu reset. Manually manipulating DOM. Ususally bad thing to do, but in this case it's ok
+    // or go back to useState
+
+    //Refs are better if you trying to read value without using much later
+    nameInputRef.current.value = '';
+    ageInputRef.current.value = '';
   };
 
-  const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value);
-  };
+//   const usernameChangeHandler = (event) => {
+//     setEnteredUsername(event.target.value);
+//   };
 
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
-  };
+//   const ageChangeHandler = (event) => {
+//     setEnteredAge(event.target.value);
+//   };
 
   const errorHandler = () =>{
     setError(null);
@@ -62,15 +74,18 @@ const AddUser = (props) => {
           <input
             id="username"
             type="text"
-            value={enteredUsername}
-            onChange={usernameChangeHandler}
+            // value={enteredName}
+            // onChange={usernameChangeHandler}
+            // connecting ref to element
+            ref={nameInputRef}
           />
           <label htmlFor="age">Age (Years)</label>
           <input
             id="age"
             type="number"
-            value={enteredAge}
-            onChange={ageChangeHandler}
+            // value={enteredUserAge}
+            // onChange={ageChangeHandler}
+            ref={ageInputRef}
           />
           <Button type="submit">Add User</Button>
         </form>
